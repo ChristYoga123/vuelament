19a7b1507bb012779580e0e833a66dfe461b4c69
ChristYoga123 <christianuswibisono@gmail.com>
2026-03-14 16:33:31 +0700

    fix(table): ImageCell now automatically prepends /storage/ to relative paths

--- resources/js/components/vuelament/table/columns/ImageCell.vue
+++ resources/js/components/vuelament/table/columns/ImageCell.vue
@@ -1,15 +1,24 @@
 <script setup>
+import { computed } from 'vue'
+
 const props = defineProps({
   row: { type: Object, required: true },
   col: { type: Object, required: true },
+})
+
+const imageUrl = computed(() => {
+  const path = props.row[props.col.name]
+  if (!path) return null
+  if (path.startsWith('http') || path.startsWith('/')) return path
+  return `/storage/${path}`
 })
 </script>
 
 <template>
   <div class="flex items-center">
     <img
-      v-if="row[col.name]"
-      :src="row[col.name]"
+      v-if="imageUrl"
+      :src="imageUrl"
       :style="{
         width: col.size || '40px',
         height: col.size || '40px',
